8f9f85ac8e14a3e1f81ccbd27acfcd9ecc4bf5ea
Vipin Devan <vipindevan08@gmail.com>
2023-06-08 11:44:21 +0200

    feat initial

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navbar = () => {
+  return <div className="w-full h-12 bg-emerald-500 sticky top-0 flex items-center font-lora">Navbar</div>;
+};
+
+export default Navbar;
