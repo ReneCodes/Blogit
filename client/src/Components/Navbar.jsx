3a9282f69de8b8121769c1b442ec0b70eb668ae4
Vipin Devan <vipindevan08@gmail.com>
2023-06-08 20:57:51 +0200

    feat: logo added

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -3,11 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import gpic from '../images/gpic.jpg';
+import logo from '../images/logo.png';
 const Navbar = () => {
   return (
-    <div className="w-full h-12 bg-emerald-500 sticky top-0 flex items-center font-lora">
+    <div className="w-full h-12  bg-white sticky top-0 flex items-center font-lora">
       <div style={{ flex: 3 }} className="flex items-center justify-center">
-        <FontAwesomeIcon className="cursor-pointer" icon={faMagnifyingGlass} />
+        <img src={logo} className="w-30 h-10" alt="logo" />
       </div>
       <div style={{ flex: 6 }}>
         <ul className="flex justify-center m-0 p-0 list-none">
@@ -16,7 +17,9 @@ const Navbar = () => {
           <li className="cursor-pointer font-light text-lg mx-8">LOGOUT</li>
         </ul>
       </div>
-      <div style={{ flex: 3 }} className="flex flex-col items-end mx-8">
+      <div style={{ flex: 3 }} className="flex items-end h-4/6">
+        <FontAwesomeIcon className="cursor-pointer w-5 h-7" icon={faMagnifyingGlass} />
+        <input type="text" placeholder="Search here" className="h-full rounded-md mr-5 ml-2 w-24" />
         <img src={gpic} className="w-10 h-10 rounded-full flex items-center justify-center" alt="profilepic" />
       </div>
     </div>
