57ec52d2a4c000761d62ccd26684528f9f46fee2
Vipin Devan <vipindevan08@gmail.com>
2023-06-08 19:23:09 +0200

    feat:create post

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -1,16 +1,18 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import gpic from '../images/gpic.jpg';
 const Create = () => {
   return (
-    <div className="pt-12">
-      <form className="writeForm">
-        <div className="flex flex-col ml-36">
+    <div className="p-12 pr-16">
+      <img src={gpic} className="ml-36 h-64 w-[70vw] object-cover rounded-md mb-5" alt="profilepic" />
+      <form className="w-[70vw] ">
+        <div className="flex flex-col relative ml-36 w-[70vw] mb-5">
           <input type="file" />
-          <input type="title" placeholder={'Title'} autoFocus={true} />
+          <input type="title" placeholder={'Title'} autoFocus={true} className="text-lg p-2 w-[70vw] mt-5" />
+          <button className="absolute top-0 right-0  bg-lime-600 rounded-lg p-1 text-white text-sm">publish</button>
         </div>
-        <div className="ml-36 border-none">
-          <ReactQuill placeholder="create your blog.." className="border-none " />
-          <button className="">publish</button>
+        <div className="border-none ml-36 w-[70vw] ">
+          <ReactQuill placeholder="create your blog.." className="border-none  h-60" />
         </div>
       </form>
     </div>
