81140234815e0acae2fb28b19b8fcdc61c9f39f8
Marko Grahovac <m_grahovac@yahoo.com>
2023-06-16 15:46:17 +0200

    index.ts

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const cookieParser = require('cookie-parser');
-const app = express();
-const cors = require('cors');
-const path = require('path');
-const port = process.env.PORT;
-
-const router = require('./router');
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv = require("dotenv");
+dotenv.config();
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var cors = require("cors");
+var path = require("path");
+var app = express();
+var port = process.env.PORT;
+var router = require("./router");
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/images', express.static(path.join(__dirname, '/images')));
+app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(router);
-
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+app.listen(port, function () {
+    console.log("app listening on port ".concat(port));
 });
