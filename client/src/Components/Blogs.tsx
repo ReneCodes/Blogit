d7ec9261ae622b9cd901f22cec52778e91359482
Rene G <r.gromeier@gmail.com>
2023-06-18 00:36:17 +0200

    chore: Convert to Typescript

--- client/src/Components/Blogs.tsx
+++ client/src/Components/Blogs.tsx
@@ -1,34 +1,45 @@
 import Blog from './Blog';
 
-import { useEffect, useState, FC } from 'react';
-import { useLocation, useNavigate, NavigateFunction } from 'react-router-dom';
-import { searchBlog } from '../utils/BlogUtils';
-import { BlogInterface } from '../@types/model';
-
+import {useEffect, useState, FC} from 'react';
+import {useLocation, useNavigate, NavigateFunction} from 'react-router-dom';
+import {searchBlog} from '../utils/BlogUtils';
+import {BlogInterface} from '../@types/model';
 
 const Blogs: FC = () => {
+	let navigate: NavigateFunction = useNavigate();
+	const [blogs, setBlogs] = useState<BlogInterface[]>([]);
+	const {search} = useLocation();
 
+	useEffect(() => {
+		searchBlog(search, setBlogs, navigate);
+	}, [search]);
 
-  let navigate: NavigateFunction = useNavigate();
-  const [blogs, setBlogs] = useState<BlogInterface[]>([]);
-  const { search } = useLocation();
-
-  useEffect(() => {
-    searchBlog(search, setBlogs, navigate);
-  }, [search]);
-
-  return (
-    <div style={{ flex: 9 }}>
-      {blogs.length === 0 &&
-        <div className='flex flex-col m-5 p-5 align-center justify-center w-1/2'>
-          <h2 className='m-auto font-semibold text-2xl	cursor-pointer '>
-            No posts to show, yet. Create one <span onClick={() => navigate('/create')} className='text-cyan-400'>here</span>.
-          </h2>
-        </div>
-      }
-      <div className="flex flex-wrap m-5">{blogs && blogs.map(( blog: BlogInterface ) => <Blog blog={blog} key={blog._id} />)}</div>
-    </div>
-  );
+	return (
+		<div style={{flex: 9}}>
+			{blogs.length === 0 && (
+				<div className="flex flex-col m-5 p-5 align-center justify-center w-1/2">
+					<h2 className="m-auto font-semibold text-2xl	cursor-pointer ">
+						No posts to show, yet. Create one{' '}
+						<span
+							onClick={() => navigate('/create')}
+							className="text-cyan-400">
+							here
+						</span>
+						.
+					</h2>
+				</div>
+			)}
+			<div className="flex flex-wrap m-5">
+				{blogs &&
+					blogs.map((blog: BlogInterface) => (
+						<Blog
+							blog={blog}
+							key={blog._id}
+						/>
+					))}
+			</div>
+		</div>
+	);
 };
 
 export default Blogs;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,65 +1,74 @@
-import { Link, NavigateFunction, useNavigate } from 'react-router-dom';
-import React, { useState, useContext, useEffect, FC } from 'react';
+import {Link, NavigateFunction, useNavigate} from 'react-router-dom';
+import React, {useState, useContext, useEffect, FC} from 'react';
 
-import { AuthContext } from '../App';
-import { fetchAuthUser, loginUser } from '../utils/AuthUtils';
-import { AuthContextType } from '../@types/auth';
+import {AuthContext} from '../App';
+import {fetchAuthUser, loginUser} from '../utils/AuthUtils';
+import {AuthContextType} from '../@types/auth';
 
 const Login: FC = () => {
-  let navigate: NavigateFunction = useNavigate();
+	let navigate: NavigateFunction = useNavigate();
 
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const { setReload, setAuth } = useContext<AuthContextType>(AuthContext);
+	const [username, setUsername] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const {setReload, setAuth} = useContext<AuthContextType>(AuthContext);
 
-  // CHECKS IF THE USER IS ALREADY AUTHENTICATED WHEN OPENING THE /LOGIN PAGE
-  useEffect(() => {
-    fetchAuthUser(setAuth, setReload, navigate);
-  }, [])
+	// CHECKS IF THE USER IS ALREADY AUTHENTICATED WHEN OPENING THE /LOGIN PAGE
+	useEffect(() => {
+		fetchAuthUser(setAuth, setReload, navigate);
+	}, []);
 
-  function handleLogin(e: React.FormEvent) {
-    e.preventDefault();
-    loginUser({ username, password }, setReload, navigate, setAuth);
-  }
+	function handleLogin(e: React.FormEvent) {
+		e.preventDefault();
+		loginUser({username, password}, setReload, navigate, setAuth);
+	}
 
-  type InputTuple = [string,string,string, React.Dispatch<React.SetStateAction<string>>]
+	type InputTuple = [string, string, string, React.Dispatch<React.SetStateAction<string>>];
 
-  const inputs: [InputTuple,InputTuple] = [
-    ['Username', 'text', username, setUsername],
-    ['Password', 'password', password, setPassword],
-  ];
+	const inputs: [InputTuple, InputTuple] = [
+		['Username', 'text', username, setUsername],
+		['Password', 'password', password, setPassword],
+	];
 
-  const allInputs = inputs.map(([item, type, state, setter]) => {
-    return (
-      <div className='flex flex-col' key={item}>
-        <label className="my-2.5 mx-0 flex flex-col">{item}
-          <input
-            className="p-2.5 my-2 bg-white border-b-2 outline-none focus:border-b-gray-500"
-            type={type}
-            placeholder={`Enter your ${item}...`}
-            value={state}
-            onChange={(e) => setter(e.target.value)}
-            />
-        </label>
-      </div>
-    )
-  });
+	const allInputs = inputs.map(([item, type, state, setter]) => {
+		return (
+			<div
+				className="flex flex-col"
+				key={item}>
+				<label className="my-2.5 mx-0 flex flex-col">
+					{item}
+					<input
+						className="p-2.5 my-2 bg-white border-b-2 outline-none focus:border-b-gray-500"
+						type={type}
+						placeholder={`Enter your ${item}...`}
+						value={state}
+						onChange={(e) => setter(e.target.value)}
+					/>
+				</label>
+			</div>
+		);
+	});
 
-  return (
-    <div className="flex flex-col items-center justify-center mt-10">
-      <span className="text-5xl">Login</span>
-      <form className="mt-5 flex flex-col" onSubmit={handleLogin}>
-        {allInputs}
-        <button className="mt-5 cursor-pointer bg-lime-600 text-white p-2.5 border-none rounded-lg text-center hover:bg-lime-700 hover:scale-[1.01] duration-500 active:hover:bg-lime-800 active:scale-[0.98]">Login</button>
-        <span className="mt-2.5">
-          Don't you have an account?{' '}
-          <Link className="ml-1 text-red-600" to="/register">
-            Register
-          </Link>
-        </span>
-      </form>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center mt-10">
+			<span className="text-5xl">Login</span>
+			<form
+				className="mt-5 flex flex-col"
+				onSubmit={handleLogin}>
+				{allInputs}
+				<button className="mt-5 cursor-pointer bg-lime-600 text-white p-2.5 border-none rounded-lg text-center hover:bg-lime-700 hover:scale-[1.01] duration-500 active:hover:bg-lime-800 active:scale-[0.98]">
+					Login
+				</button>
+				<span className="mt-2.5">
+					Don't you have an account?{' '}
+					<Link
+						className="ml-1 text-red-600"
+						to="/register">
+						Register
+					</Link>
+				</span>
+			</form>
+		</div>
+	);
 };
 
 export default Login;
