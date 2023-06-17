218f81cbbd22a6edbfc45c2eb287883132589822
Marko Grahovac <m_grahovac@yahoo.com>
2023-06-17 13:48:09 +0200

    Blog.ts

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,16 +1,12 @@
-const mongoose = require('./index');
-const { Schema } = mongoose;
-const BlogSchema = Schema(
-  {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = require("mongoose");
+var blogSchema = new mongoose_1.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     image: { type: String },
     category: { type: String },
-    author: { type: Schema.Types.ObjectId, ref: 'User' },
-  },
-  { timestamps: true }
-);
-
-const Blog = mongoose.model('Blog', BlogSchema);
-
+    author: { type: mongoose_1.Schema.Types.ObjectId, ref: "User", required: true },
+}, { timestamps: true });
+var Blog = mongoose_1.default.model("Blog", blogSchema);
 module.exports = Blog;
---
+++ server/models/Blog.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IBlog extends Document {
+  title: string;
+  content: string;
+  category?: string;
+  image?: string;
+  author: Schema.Types.ObjectId;
+}
+
+const blogSchema: Schema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    image: { type: String },
+    category: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model<IBlog>("Blog", blogSchema);
+module.exports = Blog;
