0d8c42b9df691dd1b12d1348699ad9a3482ce837
Rene G <r.gromeier@gmail.com>
2023-06-19 15:51:46 +0200

    feat: all pages and components now typescript

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import {GoogleOAuthProvider} from '@react-oauth/google';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <GoogleOAuthProvider clientId="499744852892-skivubqevd7mk9jmclv43se4n23d61nt.apps.googleusercontent.com">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </GoogleOAuthProvider>
+	<GoogleOAuthProvider clientId="499744852892-skivubqevd7mk9jmclv43se4n23d61nt.apps.googleusercontent.com">
+		{/* <React.StrictMode> */}
+		<App />
+		{/* </React.StrictMode> */}
+	</GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
