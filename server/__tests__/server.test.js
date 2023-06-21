f14d7a98805b038fcc55263b254a79967b911112
Marko Grahovac <m_grahovac@yahoo.com>
2023-06-21 18:39:04 +0200

    2+2=4

--- server/__tests__/server.test.js
+++ server/__tests__/server.test.js
@@ -1,2 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const supertest_1 = __importDefault(require("supertest"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const index_1 = __importDefault(require("../models/index"));
+const server_1 = require("../server");
+const User_1 = __importDefault(require("../models/User"));
+beforeAll(index_1.default.disconnect);
+beforeEach(() => __awaiter(void 0, void 0, void 0, function* () {
+    const testDBUrl = "mongodb://127.0.0.1:27017/testingBE";
+    yield index_1.default.connect(testDBUrl);
+}));
+afterEach(() => __awaiter(void 0, void 0, void 0, function* () {
+    if (index_1.default.connection.readyState !== 0) {
+        yield User_1.default.deleteMany({});
+        yield index_1.default.connection.close();
+    }
+}));
+const request = (0, supertest_1.default)(server_1.app);
+test("Adding 2 and 2 should be 4", () => {
+    expect(2 + 2).toBe(4);
+});
--- server/__tests__/server.test.ts
+++ server/__tests__/server.test.ts
@@ -1,2 +1,26 @@
 import supertest from "supertest";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "../models/index";
 import { app } from "../server";
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+
+beforeAll(mongoose.disconnect);
+
+beforeEach(async () => {
+  const testDBUrl = "mongodb://127.0.0.1:27017/testingBE";
+  await mongoose.connect(testDBUrl);
+});
+afterEach(async () => {
+  if (mongoose.connection.readyState !== 0) {
+    await User.deleteMany({});
+    await mongoose.connection.close();
+  }
+});
+
+const request = supertest(app);
+
+test("Adding 2 and 2 should be 4", () => {
+  expect(2 + 2).toBe(4);
+});
