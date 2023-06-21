1cb03a119593deb427b6c17b0314b64d57bf5caa
Rene G <r.gromeier@gmail.com>
2023-06-21 09:07:23 +0200

    feat: navbar with hover, sidebar blogs have shorter blurb,  profile handles file input

--- client/src/Components/SidebarContent.tsx
+++ client/src/Components/SidebarContent.tsx
@@ -1,29 +1,48 @@
-import { FC } from 'react';
-import { Link } from 'react-router-dom';
-import { BlogInterface } from '../@types/model';
+import {FC} from 'react';
+import {Link} from 'react-router-dom';
+import {BlogInterface} from '../@types/model';
 
 interface Props {
-  key: string,
-  blog: BlogInterface
+	key: string;
+	blog: BlogInterface;
 }
 
-const SidebarContent : FC<Props> = (props) => {
-  const { title, _id, content, image } = props.blog;
-  const folder = process.env.REACT_APP_IMAGE_URL;
-  return (
-    <>
-      <div className="sidebarItem">
-        <Link to={`/blog/${_id}`}>
-          <img src={folder+ '/' + image} className="mt-3.5 w-full h-44 object-cover rounded-md" alt="profilepic" />
-        </Link>
-        <p className="text-sm items-center">
-          <Link to={`/blog/${_id}`}>{title}</Link>
-        </p>
+const SidebarContent: FC<Props> = (props) => {
+	const {title, _id, content, image} = props.blog;
+	const folder = process.env.REACT_APP_IMAGE_URL;
 
-        <div className="line-clamp-4 w-4/5 mt-2 mb-5 text-xs" dangerouslySetInnerHTML={{ __html: content }} />
-      </div>
-    </>
-  );
+	function extractBlogBlurb() {
+		const contentArray = content
+			.replace(/<[^>]+>/gi, '|')
+			.split('|')
+			.filter((item) => item.length > 0);
+
+		return contentArray[0].slice(0, 99);
+	}
+
+	// console.log(content);
+	return (
+		<>
+			<div className="sidebarItem my-5 ">
+				<Link to={`/blog/${_id}`}>
+					<img
+						src={folder + '/' + image}
+						className="w-full h-44 object-cover rounded-md"
+						alt="profilepic"
+					/>
+				</Link>
+				<div className="w-full p-2 text-center bg-white rounded-b-md ">
+					<p className="text-sm items-center font-bold">
+						<Link to={`/blog/${_id}`}>{title}</Link>
+					</p>
+
+					<div className="line-clamp-4 w-full my-2 text-xs">
+						<p>{extractBlogBlurb()}</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default SidebarContent;
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useState, useContext, useEffect, ChangeEvent} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faUserPen} from '@fortawesome/free-solid-svg-icons';
 import {NavigateFunction, useNavigate} from 'react-router-dom';
@@ -13,9 +13,9 @@ import {AuthContextType} from '../@types/auth';
 const Profile: React.FC = () => {
 	const folder = process.env.REACT_APP_IMAGE_URL;
 	const [file, setFile] = useState<File>({} as File);
-	const [username, setUsername] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const [username, setUsername] = useState<string>('');
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
 	const {auth, setAuth, setReload} = useContext<AuthContextType>(AuthContext);
 	let navigate: NavigateFunction = useNavigate();
 
@@ -41,20 +41,20 @@ const Profile: React.FC = () => {
 		deleteUser(auth, navigate);
 	};
 
-	function handleFileInput(e: React.ChangeEvent<HTMLInputElement>) {
-		const files = e.target.files;
-		if (files) {
-			setFile(files[0]);
+	const handleFileInput = (e: ChangeEvent<HTMLInputElement>) => {
+		const getFiles = e.target.files;
+		if (getFiles?.length) {
+			setFile(getFiles[0]);
 		}
-	}
+	};
 
 	return (
 		<div className="mt-16 relative flex justify-center align-middle">
 			<div className="relative w-6/12 p-5">
-				<div className="flex ">
-					<span className=" text-3xl mb-5 text-fuchsia-900 ">Update Your Account</span>
+				<div className="flex justify-between ">
+					<span className=" text-3xl mb-5 text-sky-600 ">Update Your Account</span>
 					<button
-						className="w-40 ml-40 self-center border-none rounded-lg p-2.5 mt-5 cursor-pointer bg-rose-500 hover:bg-pink-950"
+						className="w-30  self-center rounded-lg p-2.5 cursor-pointer border-2 border-red-500 bg-inherit hover:bg-red-500 hover:text-white transition-all duration-200"
 						onClick={handleDelete}>
 						Delete Account
 					</button>
@@ -69,18 +69,21 @@ const Profile: React.FC = () => {
 							className=" w-16 h-16 rounded-2xl object-cover"
 							alt=""
 						/>
-						<label className="text-lg mt-5">
+						<label
+							className="text-lg mt-5"
+							htmlFor="fileInput">
 							<FontAwesomeIcon
 								icon={faUserPen}
 								className="cursor-pointer  ml-3"
 							/>
-							<input
-								className="mt-2 mb-2 h-5 border-none border-b-gray-500"
-								type="file"
-								style={{display: 'none'}}
-								onChange={(e) => handleFileInput(e)}
-							/>
 						</label>
+						<input
+							className="mt-2 mb-2 h-5 border-none border-b-gray-500"
+							type="file"
+							id="fileInput"
+							style={{display: 'none'}}
+							onChange={(e) => handleFileInput(e)}
+						/>
 					</div>
 					<label className="text-lg mt-5 flex flex-col">
 						Username
@@ -110,7 +113,7 @@ const Profile: React.FC = () => {
 						/>
 					</label>
 					<button
-						className=" w-40 self-center border-none rounded-lg p-2.5 mt-5 cursor-pointer bg-cyan-500 hover:bg-cyan-600 "
+						className=" w-40 self-center border-none rounded-lg p-2.5 mt-5 cursor-pointer bg-sky-600 hover:bg-sky-700 text-white transition-all duration-200 "
 						type="submit">
 						Update
 					</button>
